9922ce73fe6ecc00997fb1bdc952ef782744c184
Rob Ranf <robranf@gmail.com>
2021-01-27 15:46:57 -0800

    Initial Commit

---
+++ Project6b/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project6b
+//
+//  Created by Rob Ranf on 1/27/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
